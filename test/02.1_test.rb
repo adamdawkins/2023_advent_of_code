d958fbf328b1b8f6fa9f77c3488f4d70ebac5034
adamdawkins <adam@dragondrop.uk>
2023-12-02 11:41:45 +0000

    02.1 Refactor test file

--- test/02.1_test.rb
+++ test/02.1_test.rb
@@ -1,30 +1,31 @@
 require "minitest/autorun"
 require_relative "../lib/02.1"
 
-class CubeConundrumTest < Minitest::Test
-
-  def test_game_id
+class GameTest < Minitest::Test
+  def test_id
     game = Game.new("Game 1000: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
     assert_equal(1000, game.id)
   end
 
-  def test_draw_data
-    assert_equal({ red: 4, green: 0, blue: 3 }, Draw.new("3 blue, 4 red").send(:data))
-    assert_equal({ red: 1, green: 2, blue: 6 }, Draw.new("1 red, 2 green, 6 blue").send(:data))
-    assert_equal({ red: 0, green: 2, blue: 0 }, Draw.new("2 green").send(:data))
+  def test_max_colours
+    game = Game.new("Game 1000: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+    assert_equal(4, game.max_red)
+    assert_equal(2, game.max_green)
+    assert_equal(6, game.max_blue)
   end
+end
 
-  def test_draw_colours
+class DrawTest < Minitest::Test
+  def test_colours
     draw = Draw.new("1 red, 2 green, 6 blue")
     assert_equal(1, draw.red)
     assert_equal(2, draw.green)
     assert_equal(6, draw.blue)
   end
 
-  def test_game_max_colours
-    game = Game.new("Game 1000: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
-    assert_equal(4, game.max_red)
-    assert_equal(2, game.max_green)
-    assert_equal(6, game.max_blue)
+  def test_data
+    assert_equal({ red: 4, green: 0, blue: 3 }, Draw.new("3 blue, 4 red").send(:data))
+    assert_equal({ red: 1, green: 2, blue: 6 }, Draw.new("1 red, 2 green, 6 blue").send(:data))
+    assert_equal({ red: 0, green: 2, blue: 0 }, Draw.new("2 green").send(:data))
   end
 end
