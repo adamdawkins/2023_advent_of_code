3f0969419272326f65cc46579c7aefd44055cdd0
adamdawkins <adam@dragondrop.uk>
2023-12-03 13:11:16 +0000

    03.1 Add Number, #coordinates, #adjacent_coordinates

--- lib/03.1.rb
+++ lib/03.1.rb
@@ -1,3 +1,32 @@
+class Number
+  attr_reader :value, :y, :min_x, :max_x
+  def initialize(value:, y:, xs:)
+    @value = value
+    @y = y
+    @min_x, @max_x = xs
+  end
+
+  def coordinates
+    [[y, min_x], [y, max_x]]
+  end
+
+  def adjacent_coordinates
+    result = []
+    ys = [y - 1,
+          y,
+          y + 1]
+    xs = ((min_x - 1)..(max_x + 1)).to_a
+    
+    ys.each do |y|
+      xs.each do |x|
+        result << [y, x]
+      end
+    end
+
+    result - coordinates
+  end
+end
+
 class EngineSchematic
   attr_reader :input
 
--- test/03.1_test.rb
+++ test/03.1_test.rb
@@ -1,6 +1,28 @@
 require "minitest/autorun"
 require_relative "../lib/03.1"
 
+class NumberTest < Minitest::Test
+  def test_coordinates
+    number = Number.new(value: 35, y: 2, xs: [2, 3])
+    assert_equal([[2,2], [2,3]], number.coordinates)
+  end
+
+  def test_adjacent_coordinates
+    # ** = number
+    # x  = adjacent coordinate
+    # 0 0123456789A
+    # 1 0xxxx56789A
+    # 2 0x**x56789A
+    # 3 0xxxx56789A
+
+    number = Number.new(value: 35, y: 2, xs: [2, 3])
+    assert_equal([[1,1], [1,2], [1,3], [1,4],
+                  [2,1], [2,4],
+                  [3,1], [3,2], [3,3], [3,4]], number.adjacent_coordinates)
+
+  end
+end
+
 class EngineSchematicTest < Minitest::Test
   def setup
     @input = ["467..114..",
