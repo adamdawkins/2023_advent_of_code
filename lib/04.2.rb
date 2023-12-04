357d26c7a78d092f5c295a9b0faa49495a493b98
adamdawkins <adam@dragondrop.uk>
2023-12-04 17:16:55 +0000

    04.2 Add shell Pile#total_cards

---
+++ lib/04.2.rb
@@ -0,0 +1,49 @@
+class Card
+  attr_reader :id, :input
+
+  def initialize(input)
+    @input = input
+    @id = input.match(/Card\s*(\d+)/)[1]
+    @prize_input, @number_input = input.split(":").last.split(" | ")
+  end
+
+  def prize_numbers
+    extract_numbers(prize_input)
+  end
+
+  def numbers
+    extract_numbers(number_input)
+  end
+
+  def winning_numbers
+    prize_numbers & numbers
+  end
+
+  def points
+    return 0 if winning_numbers.empty?
+
+    Array.new(winning_numbers.count - 1).inject(1) {|result, _n| result * 2}
+  end
+
+  private
+
+  attr_reader :prize_input, :number_input
+
+  def extract_numbers(string)
+    string.scan(/(\d+)/).flatten.map(&:to_i)
+  end
+end
+
+class Pile
+  attr_reader :cards
+  def initialize(input)
+    @cards = input.map { |card_input| Card.new(card_input) }
+  end
+
+  def total_points
+    cards.sum(&:points)
+  end
+
+  def total_cards
+  end
+end
---
+++ test/04.2_test.rb
@@ -0,0 +1,58 @@
+require "minitest/autorun"
+require_relative "../lib/04.2"
+
+class CardTest < Minitest::Test
+# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+  def setup
+    input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+    @card = Card.new(input)
+  end
+
+  def test_id
+    assert_equal("1", @card.id)
+  end
+
+  def test_prize_numbers
+    assert_equal([41, 48, 83, 86, 17], @card.prize_numbers)
+  end
+
+  def test_numbers
+    assert_equal([83, 86, 6, 31, 17, 9, 48, 53], @card.numbers)
+  end
+
+  def test_winning_numbers
+    assert_equal([48, 83, 17, 86].sort, @card.winning_numbers.sort)
+  end
+
+  def test_points
+    # The first match makes the card worth one point and each match after the
+    # first doubles the point value of that card.
+    assert_equal(8, @card.points)
+    assert_equal(2, Card.new("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19").points)
+    assert_equal(2, Card.new("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1").points)
+    assert_equal(1, Card.new("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83").points)
+    assert_equal(0, Card.new("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36").points)
+    assert_equal(0, Card.new("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11").points)
+  end
+end
+
+class PileTest < Minitest::Test
+  def setup
+    input = ["Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+             "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+             "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+             "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+             "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+             "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"]
+    @pile = Pile.new(input)
+  end
+
+  def test_total_cards
+      assert_equal(30, @pile.total_cards)
+  end
+end
