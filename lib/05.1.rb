de7954e93472c0d8ab1dcef9f8ae76201a5d1bf3
adamdawkins <adam@dragondrop.uk>
2023-12-06 20:00:57 +0000

    05.1 Add Map, #name

--- lib/05.1.rb
+++ lib/05.1.rb
@@ -1,3 +1,17 @@
+class Map
+  def initialize(input)
+    @input = input
+  end
+
+  def name
+    input.first.match(/([\w|-]*) map:/)[1]
+  end
+
+  private
+
+  attr_reader :input
+end
+
 class Almanac
   def initialize(input)
     @input = input
