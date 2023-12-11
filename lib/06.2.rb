564b8c36c102263d32e1e471b58df9f7c03b2ae7
adamdawkins <adam@dragondrop.uk>
2023-12-11 21:38:20 +0000

    06.2 Adjust input parsing

---
+++ lib/06.2.rb
@@ -0,0 +1,56 @@
+class Race
+  attr_reader :time, :record
+
+  def initialize(time:, record:)
+    @time = time
+    @record = record
+  end
+
+  def number_of_wins
+    distances.select {|distance| distance > record }.count
+  end
+
+  def hold(ms)
+    distance = (time - ms) * ms
+  end
+
+  def distances
+    (0..time).map {|speed| hold(speed) }
+  end
+end
+
+class Racer
+  def initialize(input)
+    @times_input, @distances_input = input
+  end
+
+  def races
+    @races ||= times.zip(distances).map do |time, record|
+      Race.new(time:, record:)
+    end
+  end
+
+  def number_of_wins_per_race
+    races.map(&:number_of_wins)
+  end
+
+  def answer
+    number_of_wins_per_race.reduce(:*)
+  end
+
+  private
+
+  attr_reader :times_input, :distances_input
+
+  def times
+    extract_digits(times_input)
+  end
+
+  def distances
+    extract_digits(distances_input)
+  end
+
+  def extract_digits(string)
+    [string.scan(/\d+/).join("").to_i]
+  end
+end
---
+++ test/06.2_test.rb
@@ -0,0 +1,46 @@
+require "minitest/autorun"
+require_relative "../lib/06.2"
+
+class RacerTest < Minitest::Test
+  def setup
+    @input = ["Time:      7  15   30",
+               "Distance:  9  40  200"]
+    @racer = Racer.new(@input)
+  end
+
+  def test_races
+    assert_equal([[71530, 940200]], @racer.races.map {|race| [race.time, race.record] })
+  end
+
+  def test_number_of_wins_per_race
+    assert_equal([71503], @racer.number_of_wins_per_race)
+  end
+
+  def test_answer
+    assert_equal(71503, @racer.answer)
+  end
+end
+
+class RaceTest < Minitest::Test
+  def setup
+    @race = Race.new(time: 7, record: 9)
+  end
+  def test_hold
+    assert_equal( 0, @race.hold(0))
+    assert_equal( 6, @race.hold(1))
+    assert_equal(10, @race.hold(2))
+    assert_equal(12, @race.hold(3))
+    assert_equal(12, @race.hold(4))
+    assert_equal(10, @race.hold(5))
+    assert_equal( 6, @race.hold(6))
+    assert_equal( 0, @race.hold(7))
+  end
+
+  def test_distances
+    assert_equal([0, 6, 10, 12, 12, 10, 6, 0], @race.distances)
+  end
+
+  def test_number_of_wins
+    assert_equal(4, @race.number_of_wins)
+  end
+end
