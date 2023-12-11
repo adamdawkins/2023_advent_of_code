7831d04681146a058f8d837882066b8d9ed837d3
adamdawkins <adam@dragondrop.uk>
2023-12-11 23:16:32 +0000

    07.2 Account for J's as Jokers

---
+++ lib/07.2.rb
@@ -0,0 +1,121 @@
+require "ostruct"
+
+class Hand
+  attr_reader :cards
+
+  FIVE_OF_A_KIND  = :five_of_a_kind
+  FOUR_OF_A_KIND  = :four_of_a_kind
+  FULL_HOUSE      = :full_house
+  THREE_OF_A_KIND = :three_of_a_kind
+  TWO_PAIR        = :two_pair
+  ONE_PAIR        = :one_pair
+  HIGH_CARD       = :high_card
+
+  TYPE_RANKS = { FIVE_OF_A_KIND  => 6,
+                 FOUR_OF_A_KIND  => 5,
+                 FULL_HOUSE      => 4,
+                 THREE_OF_A_KIND => 3,
+                 TWO_PAIR        => 2,
+                 ONE_PAIR        => 1,
+                 HIGH_CARD       => 0 }
+
+  CARD_RANKS = { "A" => 12,
+                 "K" => 11,
+                 "Q" => 10,
+                 "T" =>  8,
+                 "9" =>  7,
+                 "8" =>  6,
+                 "7" =>  5,
+                 "6" =>  4,
+                 "5" =>  3,
+                 "4" =>  2,
+                 "3" =>  1,
+                 "2" =>  0,
+                 "J" =>  -1}
+
+
+  def initialize(cards)
+    @cards = cards.chars
+  end
+
+  def type
+    case counts_without_jokers.values.max + joker_count
+    when 5
+      FIVE_OF_A_KIND
+    when 4
+      FOUR_OF_A_KIND
+    when 3
+      counts.values.include?(2) ? FULL_HOUSE : THREE_OF_A_KIND
+    when 2
+      counts.values.count(2) == 2 ? TWO_PAIR : ONE_PAIR
+    else
+      HIGH_CARD
+    end
+  end
+
+  def rank
+    TYPE_RANKS[type]
+  end
+
+  def joker_count
+    cards.count("J")
+  end
+
+  def counts_without_jokers
+    cards.reject {|c| c == "J"}.tally
+  end
+
+  def <=>(other_hand)
+    type_comparison = rank <=> other_hand.rank
+    return card_comparison(other_hand) if type_comparison.zero?
+    type_comparison
+  end
+
+  def to_s
+    cards.join
+  end
+
+  private
+
+  def counts
+    cards.tally
+  end
+
+  def card_comparison(other_hand)
+    (0..4).each do |position|
+      comparison = CARD_RANKS[cards[position]] <=> CARD_RANKS[other_hand.cards[position]]
+      return comparison unless comparison.zero?
+    end
+    0
+  end
+end
+
+class CamelCards
+  def initialize(input)
+    @input = input
+  end
+
+  def table
+    @table ||= input.map do |play|
+      hand_input, bid_input = play.split(" ")
+      OpenStruct.new(hand: Hand.new(hand_input), bid: bid_input.to_i)
+    end
+  end
+
+  def order
+    table.sort_by(&:hand)
+  end
+
+  def total_winnings
+    winnings = 0
+    order.map(&:bid).each_with_index do |bid, index|
+      winnings += bid * (index + 1)
+    end
+    
+    winnings
+  end
+
+  private
+
+  attr_reader :input
+end
---
+++ test/07.2_test.rb
@@ -0,0 +1,70 @@
+require "minitest/autorun"
+require_relative "../lib/07.2"
+
+class HandTest < Minitest::Test
+  def test_type
+    assert_equal(Hand::FIVE_OF_A_KIND, Hand.new("AAAAA").type)
+    assert_equal(Hand::FOUR_OF_A_KIND, Hand.new("AA8AA").type)
+    assert_equal(Hand::FULL_HOUSE, Hand.new("23332").type)
+    assert_equal(Hand::THREE_OF_A_KIND, Hand.new("TTT98").type)
+    assert_equal(Hand::TWO_PAIR, Hand.new("23432").type)
+    assert_equal(Hand::ONE_PAIR, Hand.new("A23A4").type)
+    assert_equal(Hand::HIGH_CARD, Hand.new("23456").type)
+
+    assert_equal(Hand::FOUR_OF_A_KIND, Hand.new("QJJQ2").type)
+  end
+
+  def test_joker_count
+    assert_equal(2, Hand.new("QJJQ2").joker_count)
+  end
+
+  def test_counts_without_jokers
+    assert_equal({"Q" => 2, "2" => 1} , Hand.new("QJJQ2").counts_without_jokers)
+  end
+
+  def test_rank
+    assert_equal(6, Hand.new("AAAAA").rank)
+    assert_equal(5, Hand.new("AA8AA").rank)
+    assert_equal(4, Hand.new("23332").rank)
+    assert_equal(3, Hand.new("TTT98").rank)
+    assert_equal(2, Hand.new("23432").rank)
+    assert_equal(1, Hand.new("A23A4").rank)
+    assert_equal(0, Hand.new("23456").rank)
+  end
+
+  def test_spaceship
+    assert_equal(1, Hand.new("AAAAA")<=>(Hand.new("AA8AA")))
+    assert_equal(1, Hand.new("33332")<=>(Hand.new("2AAAA")))
+  end
+
+  def test_to_s
+    assert_equal("AAAAA", Hand.new("AAAAA").to_s)
+  end
+end
+
+class CamelCardsTest < Minitest::Test
+  def setup
+    @input = ["32T3K 765",
+              "T55J5 684",
+              "KK677 28",
+              "KTJJT 220",
+              "QQQJA 483"]
+    @camel_cards = CamelCards.new(@input)
+  end
+
+  def test_bids
+    assert_equal([765, 684, 28, 220, 483], @camel_cards.table.map(&:bid))
+  end
+
+  def test_order
+    assert_equal(["32T3K",
+                  "KK677",
+                  "T55J5",
+                  "QQQJA",
+                  "KTJJT"], @camel_cards.order.map(&:hand).map(&:to_s))
+  end
+
+  def test_total_winnings
+    assert_equal(5905, @camel_cards.total_winnings)
+  end
+end
