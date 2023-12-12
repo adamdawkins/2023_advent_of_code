163932cc7242c8e8cedb843fce6a13d47f88db1b
adamdawkins <adam@dragondrop.uk>
2023-12-12 10:40:59 +0000

    08.1 Add Wasteland#network

--- lib/08.1.rb
+++ lib/08.1.rb
@@ -2,13 +2,24 @@ class Wasteland
   def initialize(input)
     @input = input
     @directions_input = @input.first
+    @network_input = @input[2..]
   end
 
   def directions
     directions_input.chars.map {|c| c == "R" ? :right : :left }
   end
 
+  def network
+    network_input.map {|node| parse_node(node)}.to_h
+  end
+
+
   private
 
-  attr_reader :input, :directions_input
+  attr_reader :input, :directions_input, :network_input
+
+  def parse_node(node)
+    key, left, right = node.scan(/(\w*) = \((\w*), (\w*)\)/).flatten
+    [key, [left, right]]
+  end
 end
--- test/08.1_test.rb
+++ test/08.1_test.rb
@@ -5,6 +5,7 @@ class WastelandTest < Minitest::Test
   def setup
     @input = [
       "RL",
+      "",
       "AAA = (BBB, CCC)",
       "BBB = (DDD, EEE)",
       "CCC = (ZZZ, GGG)",
@@ -18,5 +19,17 @@ class WastelandTest < Minitest::Test
   def test_directions
     wasteland = Wasteland.new(@input)
     assert_equal([:right, :left], wasteland.directions)
+  end
+
+  def test_network
+    wasteland = Wasteland.new(@input)
+    assert_equal({ "AAA" => ["BBB", "CCC"],
+                   "BBB" => ["DDD", "EEE"],
+                   "CCC" => ["ZZZ", "GGG"],
+                   "DDD" => ["DDD", "DDD"],
+                   "EEE" => ["EEE", "EEE"],
+                   "GGG" => ["GGG", "GGG"],
+                   "ZZZ" => ["ZZZ", "ZZZ"]}, wasteland.network
+                )
   end
 end
